b76a49406c74e51c132c58e7afe2876c7b2c51b6
LuisCorreaDv <dev.luis.correa@gmail.com>
2025-05-07 18:38:24 -0600

    Wrap children in Provider for state management

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Provider } from "@/redux/providers";
 import "./globals.css";
 import Footer from "@/components/Footer";
 
@@ -29,7 +30,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <div className="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#7fb0f0_100%)]"></div>
-        {children}
+        <Provider>
+          {children}
+        </Provider>
         <Footer />
       </body>
     </html>
