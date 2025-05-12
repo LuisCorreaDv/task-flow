145d98bcc373b2e2c25a8001f1cf3319589e910e
LuisCorreaDv <dev.luis.correa@gmail.com>
2025-05-11 20:27:59 -0600

    Adding Header component to tasks page with logout logic

---
+++ src/app/tasks/components/Header.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { logout } from "@/redux/features/authSlice";
+import { useDispatch } from "react-redux";
+import { useRouter } from "next/navigation";
+
+function Header() {
+  const dispatch = useDispatch();
+  const router = useRouter();
+
+  const handleLogout = () => {
+    dispatch(logout());
+    router.push("/");
+  };
+  return (
+    <header>
+      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+          <a
+            href="https://flowbite.com"
+            className="flex items-center space-x-3 rtl:space-x-reverse"
+          >
+            <span className="self-center text-4xl font-semibold whitespace-nowrap dark:text-white text-center bg-gradient-to-r from-cyan-300 to-sky-800 bg-clip-text text-transparent">
+              Task Flow
+            </span>
+          </a>
+          <div className="flex items-center space-x-6 rtl:space-x-reverse">
+            <button
+              type="button"
+              onClick={handleLogout}
+              className="text-white font-medium bg-sky-800 hover:bg-sky-600 focus:outline-none focus:ring-4 focus:ring-sky-300 rounded-full text-sm px-5 py-2.5 text-center dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800 transition duration-200 ease-in-out cursor-pointer"
+            >
+              Log Out
+            </button>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,27 +1,12 @@
 'use client';
-
-import { useDispatch } from 'react-redux';
-import { useRouter } from 'next/navigation';
-import { logout } from '@/redux/features/authSlice';
+import Header from './components/Header';
 
 export default function TasksPage() {
-  const dispatch = useDispatch();
-  const router = useRouter();
-
-  const handleLogout = () => {
-    dispatch(logout());
-    router.push('/');
-  };
 
   return (
     <div className="p-4">
-      <h1 className="text-xl font-semibold mb-4">Tasks Page</h1>
-      <button
-        onClick={handleLogout}
-        className="bg-red-600 text-white px-4 py-2 rounded"
-      >
-        Log Out
-      </button>
+      <Header />
+      
     </div>
   );
 }
